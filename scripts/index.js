7c6aa2471fab3275533c4596b126747abf2f5a89
LuisVillela781 <89324870+LuisVillela781@users.noreply.github.com>
2022-01-21 17:14:22 -0600

    files

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+function noob() {
+	document.getElementById("title").innerHTML = "santiago is a noob";
+	window.alert("santiago is a nooob");
+}
+function predictionClick() {
+	const date = new Date();
+	//window.alert("santiago will take a shit at: " + date);
+	document.getElementById("prediction").innerHTML = "santiago will take a shit at: " + date;
+}
