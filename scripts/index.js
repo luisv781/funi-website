fad510dbcee71cb5d5e1c5f764b2dd90d4002fc6
luisv781 <89324870+luisv781@users.noreply.github.com>
2022-01-25 17:21:27 -0600

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,23 @@
 function noob() {
-	document.getElementById("title").innerHTML = "santiago is a noob";
-	window.alert("santiago is a nooob");
+  document.getElementById("title").innerHTML = "santiago is a noob";
+  window.alert("santiago is a nooob");
 }
 function predictionClick() {
-	const date = new Date();
-	//window.alert("santiago will take a shit at: " + date);
-	const customDate = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes() + 5);
-	document.getElementById("prediction").innerHTML = "santiago will take a shit at: " + customDate.toLocaleString();
+  //prediction 1
+  const date = new Date();
+  //window.alert("santiago will take a shit at: " + date);
+  const pred1 = "santiago will take a shit at: " + new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes() + 5).toLocaleString();
+  
+  //prediction 2
+  const pred2 = "schools will shutdown again on March 15, 2022"
+
+  //choose prediction
+  const rand = Math.floor(Math.random() * 2);
+  
+  if (rand == 0) {
+    document.getElementById("prediction").innerHTML = pred1;
+  } else if (rand == 1) {
+    document.getElementById("prediction").innerHTML = pred2;
+  }
+  
 }
